8a92fa47ce1ff4cde59e271fa0f2673694bd34b0
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2023-07-27 23:10:46 +0530

    Update Array insertion deletion.c

--- Array insertion deletion.c
+++ Array insertion deletion.c
@@ -56,7 +56,7 @@ int main()
 	printf("enter the size of the array: ");
 	scanf("%d",&n);
 
-	int* a=(int)malloc(n*sizeof(int));
+	int* a=(int*)malloc(n*sizeof(int));
 
 	printf("enter the elements of the array: ");
 	for(i=0;i<n;i++) {
