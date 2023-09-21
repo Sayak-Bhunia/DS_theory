6e31955116c96d06cb0eefcacd9ddae366af04e3
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2023-09-21 12:42:52 +0530

    Create Circular Queue.c

---
+++ Circular Queue.c
@@ -0,0 +1,97 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Queue
+{
+    int *arr;
+    int size;
+    int front;
+    int rear;
+};
+
+struct Queue create(int size)
+{
+    struct Queue q;
+    q.size = size;
+    q.front = 0;
+    q.rear = 0;
+    q.arr = (int*)malloc(sizeof(int)*size);
+    if (q.arr == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    return q;
+};
+
+void enqueue(struct Queue *q)
+{
+    int val;
+    if((q->front == 1 && q->rear == q->size) || q->front == q->rear + 1) printf("\nOVERFLOW\n");
+    else {
+        if(q->front == 0 && q->rear == 0) q->front = q->rear = 1;
+        else if(q->rear == q->size) q->rear = 1;
+        else (q->rear)++;
+        printf("ENTER ELEMENT TO INSERT: ");
+        scanf("%d",&val);
+        q->arr[q->rear] = val;
+    }
+}
+
+void dequeue(struct Queue *q)
+{
+    if((q->front) == 0) printf("\nQUEUE EMPTY!\n"); //means rear = 0 also.
+    else if(q->front == q->size) q->front = 1;
+    else if((q->front) == (q->rear)) { //QUEUE contains only one element.
+        q->front = q->rear = 0;
+    }
+    else (q->front)++;
+}
+
+void display(struct Queue *q)
+{
+    int i;
+    if((q->front) == 0) printf("\nQUEUE EMPTY!\n");
+    else{
+        printf("\nQUEUE ELEMENTS:\n");
+        for(i=q->front;i!=q->rear;i=(i+1)%(q->size)) {
+            printf("%d ",q->arr[i]);
+        }
+        printf("%d ",q->arr[i]);
+        printf("\n");
+    }
+}
+
+int main()
+{
+    struct Queue q;
+    int ch,val;
+    printf("ENTER SIZE: ");
+    scanf("%d",&q.size);
+
+    q = create(q.size);
+
+    while (1) {
+        printf("1->INSERT\n");
+        printf("2->DELETE\n");
+        printf("3->DISPLAY\n");
+        printf("4->EXIT\n");
+        printf("Enter your choice: ");
+        scanf("%d", &ch);
+
+        switch (ch) {
+        case 1:
+                enqueue(&q);
+                break;
+        case 2: dequeue(&q);
+                break;
+        case 3: display(&q);
+                break;
+        case 4: free(q.arr);
+                exit(0);
+                break;
+        default:
+            printf("INVALID CHOICE!\n");
+        }
+    }
+    return 0;
+}
