9ab7b19ce752a04a7fb777fbb89b5d679d04fb33
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2023-11-03 11:51:07 +0530

    Update Binary Tree Implementation using Linked List.c

--- Binary Tree Implementation using Linked List.c
+++ Binary Tree Implementation using Linked List.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 typedef struct TreeNode
 {
@@ -8,7 +8,8 @@ typedef struct TreeNode
     struct TreeNode *right;
 }TreeNode;
 
-TreeNode* createNode(int x) {
+TreeNode* createNode(int x)
+{
     TreeNode* newNode = (TreeNode*)malloc(sizeof(TreeNode));
     newNode->val = x;
     newNode->left = NULL;
@@ -17,84 +18,91 @@ TreeNode* createNode(int x) {
     return newNode;
 }
 
-TreeNode* insertLeft(TreeNode* root, int x)
+TreeNode* constarr(int arr[],int i,int size)
+{
+    TreeNode* newNode = NULL;
+    if(i<size) {
+        newNode = createNode(arr[i]);
+        newNode->left = constarr(arr, 2*i+1, size);
+        newNode->right = constarr(arr, 2*i+2,size);
+    }
+
+    return newNode;
+}
+
+TreeNode* insert_left(TreeNode* root,int x)
 {
     root->left = createNode(x);
     return root->left;
 }
 
-TreeNode* insertRight(TreeNode* root, int x)
+TreeNode* insert_right(TreeNode* root,int x)
 {
     root->right = createNode(x);
     return root->right;
 }
 
-void inorder(TreeNode* root) {
-    if (root == NULL) return;
+void inorder(TreeNode* root)
+{
+    if(root == NULL) return;
     inorder(root->left);
-    printf("%d ", root->val);
+    printf("%d ",root->val);
     inorder(root->right);
 }
 
-void preorder(TreeNode* root) {
-    if (root == NULL) return;
-    printf("%d ", root->val);
+void preorder(TreeNode* root)
+{
+    if(root == NULL) return;
+    printf("%d ",root->val);
     preorder(root->left);
     preorder(root->right);
 }
 
-void postorder(TreeNode* root) {
-    if (root == NULL) return;
+void postorder(TreeNode* root)
+{
+    if(root == NULL) return;
     postorder(root->left);
     postorder(root->right);
-    printf("%d ", root->val);
+    printf("%d ",root->val);
 }
 
 int main()
 {
-    int parent, ch, n;
-    printf("Enter root: ");
-    scanf("%d", &parent);
-    TreeNode *root = createNode(parent);
-    while (1) {
-        printf("1->INSERT LEFT\n");
-        printf("2->INSERT RIGHT\n");
-        printf("3->INORDER\n");
-        printf("4->PREORDER\n");
-        printf("5->POSTORDER\n");
-        printf("6->EXIT\n");
-        printf("ENTER CHOICE: ");
-        scanf("%d", &ch);
-        switch (ch) {
-            case 1:
-                printf("ENTER NODE: ");
-                scanf("%d", &n);
-                insertLeft(root, n);
-                break;
-            case 2:
-                printf("ENTER NODE: ");
-                scanf("%d", &n);
-                insertRight(root, n);
-                break;
-            case 3:
-                printf("INORDER\n");
-                inorder(root);
-                printf("\n");
-                break;
-            case 4:
-                printf("PREORDER\n");
-                preorder(root);
-                printf("\n");
-                break;
-            case 5:
-                printf("POSTORDER\n");
-                postorder(root);
-                printf("\n");
-                break;
-            case 6:
-                exit(0);
-            default:
-                printf("\nINVALID CHOICE!\n");
+    int ch,size,i;
+    printf("Enter number of nodes: ");
+    scanf("%d",&size);
+    int arr[size];
+    printf("enter the elements: ");
+    for(i=0;i<size;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    TreeNode *root = constarr(arr,0,size);
+
+    while(1) {
+        printf("1->INORDER\n");
+        printf("2->PREORDER\n");
+        printf("3->POSTORDER\n");;
+        printf("4->EXIT\n");
+        printf("Enter choice: ");
+        scanf("%d",&ch);
+
+        switch(ch) {
+            case 1: printf("INORDER\n");
+                    inorder(root);
+                    printf("\n");
+                    break;
+            case 2: printf("PREORDER\n");
+                    preorder(root);
+                    printf("\n");
+                    break;
+            case 3: printf("POSTORDER\n");
+                    postorder(root);
+                    printf("\n");
+                    break;
+            case 4: exit(0);
+            default: printf("\nINVALID CHOICE!\n");
         }
     }
 }
